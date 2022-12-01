f9b3514ca87aa8e5b10701e70c0340f6b224ad72
TRIOLI LOW <trioli.low@gmail.com>
2022-12-01 21:59:11 +0300

    Init

---
+++ Home_work_2/Home_work_2.cpp
@@ -0,0 +1,76 @@
+﻿#include<Windows.h>
+#include <iostream>
+class Counter {
+public:
+	int num = 0;
+	int plus() {
+		return ++num;
+	}
+	int minus() {
+		return --num;
+	}
+	void sost() {
+		std::cout << num;
+	}
+	int nach() {
+		return num = 1;
+	}
+	int init(int n) {
+		num = n;
+		return num;
+	}
+};
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	int num(0);
+	std::string y = "да";
+	std::string n = "нет";
+	std::string v = "";
+	const char plus = '+';
+	const char minus = '-';
+	const char res = '=';
+	const char exit = 'x';
+	char com = ' ';
+	Counter count;
+
+	std::cout << "Вы хотите указать начальньное значение счетчика? Введите да или нет: ";
+	std::cin >> v;
+	std::cout << std::endl;
+	
+		while ((v != y) && (v != n)) {
+				std::cout << "Неккоректный ввод! Введите да или нет: ";
+				std::cin >> v;
+				std::cout << std::endl;	
+		}
+		if (v == n) {
+			count.nach();
+		}
+		else {
+			std::cout << "Введите начальное значение счетчика: ";
+			std::cin >> num;
+			count.init(num);
+		
+	}
+	while (com != exit) {
+		std::cout << "Введите команду('+', '-', '=' или 'x'): ";
+		std::cin >> com;
+		switch (com) {
+		case (plus): count.plus();
+			break;
+		case (minus): count.minus();
+			break;
+		case (res): count.sost();
+			std::cout << std::endl;
+			break;
+		case (exit): std::cout << "До свидания!";
+			break;
+		default: std::cout << "Некорректный ввод " << std::endl;
+			break;
+		}
+	}
+
+
+}
+
---
+++ Howe_work_1/Howe_work_1.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+#include <Windows.h>
+class Calculator {
+public:
+	int num1 = 0;
+	int num2 = 0;
+	double add() {
+		return num1 + num2;
+	}
+	double multiply() {
+		return num1 * num2;
+	}
+	double subtract_1_2() {
+		return num1 - num2;
+	}
+	double subtract_2_1(){
+		return num2 - num1;
+	}
+	double divide_1_2() {
+		return num1 / num2;
+	}
+	double divide_2_1() {
+		return num2 / num1;
+	}
+	bool set_num1(double num1) {
+		this->num1 = num1;
+		if (num1 != 0) {
+			num1 = true;
+		}
+		return num1;
+	}
+	bool set_num2(double num2) {
+		this->num2 = num2;
+		if (num2 != 0) {
+			num2 = true;
+		}
+		return num2;
+	}
+};
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	int num1(0);
+	int num2(0);
+	Calculator calc;
+	while (!0) {
+		std::cout << "Введите num1: ";
+		std::cin >> num1;
+		while (calc.set_num1(num1) == 0) {
+			std::cout << "Неверный ввод!\n" << "Введите num1 : ";
+			std::cin >> num1;
+		}
+		std::cout << "Введите num2: ";
+		std::cin >> num2;
+		while (calc.set_num2(num2) == 0) {
+			std::cout << "Неверный ввод!\n" << "Введите num2 : ";
+			std::cin >> num2;
+		}
+		std::cout << "num1 + num2 = " << calc.add() << "\n";
+		std::cout << "num1 - num2 = " << calc.subtract_1_2() << "\n";
+		std::cout << "num2 - num1 = " << calc.subtract_2_1() << "\n";
+		std::cout << "num1 * num2 = " << calc.multiply() << "\n";
+		std::cout << "num1 / num2 = " << calc.divide_1_2() << "\n";
+		std::cout << "num2 / num1 = " << calc.divide_2_1() << "\n";
+	}
+}
+
